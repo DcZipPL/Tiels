3ad379cd7a8e69f4cf28494d526be713cc808844
DcZipPL <dczip89@gmail.com>
2020-09-26 16:29:14 +0200

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    [System.STAThreadAttribute()]
+    public static void Main()
+    {
+        using (var uwp = new TielsUWPApp.App())
+        {
+            Tiels.App app = new Tiels.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
